5dce0c0989277c33047d952914c1a9419d018bca
Echo Ji <echojike@gmail.com>
2022-03-30 19:11:33 +1100

    created authentication component and sign in component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -7,7 +7,7 @@ const FormInput = ({ label, ...otherProps }) => {
       {label && (
         <label
           className={`${
-            otherProps.value.lengh ? "shrink" : ""
+            otherProps.value.length ? "shrink" : ""
           } form-input-label`}
         >
           {label}
